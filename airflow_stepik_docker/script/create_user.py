0bafec25e5705f386ceddc92a3ab6164a01f4fe5
Vadim Generalov <57150607+e-Wade@users.noreply.github.com>
2023-06-19 12:53:14 +0300

    Add files via upload

---
+++ airflow_stepik_docker/script/create_user.py
@@ -0,0 +1,13 @@
+from airflow import models, settings
+from airflow.contrib.auth.backends.password_auth import PasswordUser
+
+print('Creating user!!!!!!!!!!!!!!!!!!!!!!!!!!')
+user = PasswordUser(models.User())
+user.username = 'admin'
+user.email = 'new_user_email@example.com'
+user.password = '12345'
+session = settings.Session()
+session.add(user)
+session.commit()
+session.close()
+print('User created!!!!!!!!!!!!!!!!!!!!!!!!!!')
